b06dc83dce805362156f34158dbfb08d41dc927a
Omkar <omkarsmestry04@gmail.com>
2024-02-25 07:31:00 +0530

    practice files added

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+//import styles from './ProductCart.module.css'
+
+const ProductCard = () => {
+  return (
+    <div //className='p-4 
+    // my-5 
+    // bg-sky-400 
+    // text-white 
+    // text-lg 
+    // hover:bg-sky-700'
+    > 
+      <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard
